70fbb2406c28e5f7896703c79b1a422b73b09358
Krishnapol712 <krishnapol712@gmail.com>
2025-06-24 11:09:44 +0530

    Create Init_database.sql

---
+++ Scripts/Init_database.sql
@@ -0,0 +1,15 @@
+/* Creating the Database and Schemas*/
+
+Use Master
+
+Create Database DataWarehouse
+
+Use DataWarehouse
+
+Create SCHEMA Bronze
+go
+  
+Create SCHEMA Silver
+go
+  
+Create SCHEMA Gold
